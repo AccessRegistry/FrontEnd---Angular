a1ab81eb65241a90b48c16161840bfbe1810d1dc
FelipeAmorimReis <dev.felipeamorim@gmail.com>
2023-08-08 10:04:40 -0300

    [feat] adicionando validação de campos

--- egsSistemas/src/app/cadastro/cadastro.component.ts
+++ egsSistemas/src/app/cadastro/cadastro.component.ts
@@ -15,13 +15,66 @@ export class CadastroComponent {
     email: '',
     cpf: ''
   };
+  aceitouTermos = false;
 
-  constructor(private router: Router,  private dataService: DataService) {}
+  constructor(private router: Router, private dataService: DataService) { }
 
   salvar() {
-    this.dataService.dados.push(this.dados);
-
-    // Redireciona para a página de exibição dos dados cadastrados
-    this.router.navigate(['/exibicao']);
+    if (this.aceitouTermos) {
+      this.dataService.dados.push(this.dados);
+      this.router.navigate(['/exibicao']);
+    } else {
+      alert('Por favor, aceite os termos e condições.');
+    }
   }
+
+  aplicarMascaraCPF(cpf: string): string {
+    const TAMANHO_MAX_CPF = 14;
+
+    if (!cpf) {
+      return '';
+    }
+
+    cpf = cpf.replace(/\D/g, '');
+    cpf = cpf.slice(0, TAMANHO_MAX_CPF);
+
+    if (cpf.length <= 3) {
+      return cpf;
+    } else if (cpf.length <= 6) {
+      return `${cpf.slice(0, 3)}.${cpf.slice(3)}`;
+    } else if (cpf.length <= 9) {
+      return `${cpf.slice(0, 3)}.${cpf.slice(3, 6)}.${cpf.slice(6)}`;
+    } else {
+      return `${cpf.slice(0, 3)}.${cpf.slice(3, 6)}.${cpf.slice(6, 9)}-${cpf.slice(9)}`;
+    }
+  }
+
+  aplicarMascaraData(data: string): string {
+    const TAMANHO_MAX_DATA = 10;
+
+    if (!data) {
+      return '';
+    }
+
+    data = data.replace(/\D/g, '');
+    data = data.slice(0, TAMANHO_MAX_DATA);
+
+    if (data.length <= 2) {
+      return data;
+    } else if (data.length <= 4) {
+      return `${data.slice(0, 2)}/${data.slice(2)}`;
+    } else {
+      return `${data.slice(0, 2)}/${data.slice(2, 4)}/${data.slice(4)}`;
+    }
+  }
+
+  aplicarMascaraEmail(email: string): string {
+    if (!email) {
+      return '';
+    }
+
+    // Mantém apenas os caracteres permitidos em um e-mail
+    return email.replace(/[^a-zA-Z0-9@._-]/g, '');
+  }
+
 }
